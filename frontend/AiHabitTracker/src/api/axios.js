9810619f53f8c111ada832acb29d91f5c53b9511
Akil Khan <akilk6388@gmail.com>
2026-06-25 14:11:18 +0530

    Fix axios duplicate code

--- frontend/AiHabitTracker/src/api/axios.js
+++ frontend/AiHabitTracker/src/api/axios.js
@@ -1,6 +1,3 @@
-
-
-
 import axios from "axios";
 
 const api = axios.create({
@@ -8,16 +5,6 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default api;api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
